54225cbfa1e2a5ff13830343df5c4144c50aff4a
Prem Prakash Sharma <premprakashsharma9211@gmail.com>
2024-07-29 21:52:54 +0530

    refactor: update image source in Home component

--- app/page.tsx
+++ app/page.tsx
@@ -55,7 +55,7 @@ export default function Home() {
       <div className="relative m-5 z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
         <Image
           className="relative dark:drop-shadow-lg rounded-full"
-          src="/profile.png"
+          src="/images/profile.png"
           alt="Next.js Logo"
           width={180}
           height={37}

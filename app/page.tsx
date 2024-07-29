661e4a8a45e17ba01ad6974a0667d8018f5ae1ae
Prem Prakash Sharma <premprakashsharma9211@gmail.com>
2024-07-29 21:54:17 +0530

    feat: add gateway name to Home component

--- app/page.tsx
+++ app/page.tsx
@@ -71,6 +71,7 @@ export default function Home() {
             className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           >
             <h2 className="mb-3 text-2xl font-semibold">
+              {gateway.name}{" "}
               <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
                 -&gt;
               </span>

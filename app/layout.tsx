332450740b38bf24681dd0e2f6c0ba3a3b56385e
Prem Prakash Sharma <premprakashsharma9211@gmail.com>
2024-07-30 23:39:32 +0530

    chore: Add SpeedInsights component to layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
@@ -22,6 +23,7 @@ export default function RootLayout({
       <body className={`${inter.className} h-min-screen`}>
         {children}
         <ToastContainer position="top-center" draggable={true} />
+        <SpeedInsights />
         <Analytics />
       </body>
     </html>
